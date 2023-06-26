c84bb48e867a0be38b71597eb99a49381d7c6888
Ilnaz Zinnurov <zinnurov.ilnaz@gmail.com>
2023-06-26 19:54:44 +0300

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,9 +4,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY =os.environ.get("SECRET_KEY", 'django-insecure-cg6*%6d51ef8f#4!r3*vmxm4)abgjw8mo!4y-q*uq1!4-89') 
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-cg6*%6d51ef8f#4!r3*vmxm4)abgjw8mo!4y-q*uq1!4-89')
 
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'true' 
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'taski-ilnaz.myftp.biz']
 
